315f7c17266fb144697a13de5f830bbe7e680e5e
Sergei Iu <serg_omen@mail.ru>
2022-10-23 12:01:09 +0300

    Deployong on Render

--- agorapi/settings.py
+++ agorapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r)qcf$d+*&!)8^wan2dboxtv83#fel%x_(51z&mjh8)oft&%!_'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-r)qcf$d+*&!)8^wan2dboxtv83#fel%x_(51z&mjh8)oft&%!_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
