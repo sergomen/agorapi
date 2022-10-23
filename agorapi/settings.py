49bbceb8078e3e10bb9cc40ce0b117d850c653d6
Sergei Iu <serg_omen@mail.ru>
2022-10-23 13:48:09 +0300

    repaired staticfiles

--- agorapi/settings.py
+++ agorapi/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # STATIC_URL = 'static/'
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '/staticfiles/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
