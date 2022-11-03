e81275409b8b821646c8fe4dd3cf8a38a21813bb
Sergei Iu <serg_omen@mail.ru>
2022-11-03 23:07:02 +0300

    final commit

--- agorapi/settings.py
+++ agorapi/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = TRUE
 
 ALLOWED_HOSTS = ["*"]
 
@@ -137,8 +137,8 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/avatar')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/avatar')
+MEDIA_URL = '/media/'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUD_NAME'),
     'API_KEY': env('API_KEY'),
